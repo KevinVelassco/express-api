331ef5b5293759748828caeaeddee9085c13f739
Kevin Andres velasco <kevinvelassco@gmail.com>
2020-06-06 18:55:32 -0500

    add model and user migration

---
+++ models/user.js
@@ -0,0 +1,22 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+	const User = sequelize.define(
+		"User",
+		{
+			email: {
+				allowNull: false,
+				type: Sequelize.STRING,
+				unique: true
+			},
+			password_hash: {
+				type: DataTypes.STRING,
+				allowNull: false
+			}
+		},
+		{}
+	);
+	User.associate = function (models) {
+		// associations can be defined here
+	};
+	return User;
+};
